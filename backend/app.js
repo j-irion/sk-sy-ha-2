7143a6f0a0ed626586aef477151d36361ed785f0
j-irion <juliusirion@gmx.de>
2023-05-09 15:53:28 +0200

    Create express app

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const config = require('./config/config');
+
+const routes = require('./routes/routes');
+
+const app = express();
+
+mongoose.connect(config.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/todos', routes);
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(config.APP_PORT);
+
+module.exports = app;
